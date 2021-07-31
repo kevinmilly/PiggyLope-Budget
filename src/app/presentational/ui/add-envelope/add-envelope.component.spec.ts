52d5faf80a99efe2173950094c029614b923fa04
kevinmilly <kevinmilly@gmail.com>
2021-07-30 23:16:23 -0400

    updated to ng v11

--- src/app/presentational/ui/add-envelope/add-envelope.component.spec.ts
+++ src/app/presentational/ui/add-envelope/add-envelope.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { AddEnvelopeComponent } from './add-envelope.component';
 
@@ -6,7 +6,7 @@ describe('AddEnvelopeComponent', () => {
   let component: AddEnvelopeComponent;
   let fixture: ComponentFixture<AddEnvelopeComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ AddEnvelopeComponent ]
     })
--- src/app/presentational/ui/envelope/envelope.component.spec.ts
+++ src/app/presentational/ui/envelope/envelope.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { EnvelopeComponent } from './envelope.component';
 
@@ -6,7 +6,7 @@ describe('EnvelopeComponent', () => {
   let component: EnvelopeComponent;
   let fixture: ComponentFixture<EnvelopeComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ EnvelopeComponent ]
     })
